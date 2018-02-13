43bf8c5f811d2bceb728dbb618e3fb77a7f3891e
Anton_Kudryn <Anton_Kudryn@EPBYGROW0342>
2018-02-13 18:01:03 +0300

    Added interfaces for UnitOfWork and GenericRepository

---
+++ WpfOutlook/Model/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Model.Interfaces
+{
+    public interface IUnitOfWork<TEntity> : INotifyPropertyChanged where TEntity: class
+    {
+        IGenericRepository<Appointment> Managers { get; }
+        IGenericRepository<Group> Sales { get; }
+        IGenericRepository<User> Users { get; }
+        IGenericRepository<Role> Roles { get; }
+        IGenericRepository<Location> Locations { get; }
+        void Save();
+    }
+}
