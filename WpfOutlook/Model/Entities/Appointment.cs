198a383626165696b3ef6125775e59dd69069f12
Anton_Kudryn <Anton_Kudryn@EPBYGROW0342>
2018-02-13 16:21:25 +0300

    added appointment and user classes

---
+++ WpfOutlook/Model/Entities/Appointment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entities
+{
+    public class Appointment
+    {
+        public int AppointmentId { get; set; }
+        public string Subject { get; set; }
+        public DateTime BeginningDate { get; set; }
+        public DateTime EndingDate { get; set; }
+
+        public int LocationId { get; set; }
+        public Location Location { get; set; }
+    }
+}
---
+++ WpfOutlook/Model/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entities
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+    }
+}
