54785f5329443e5437db3169b27ddff5639422e9
Anton_Kudryn <Anton_Kudryn@EPBYGROW0342>
2018-02-13 16:42:40 +0300

    Added DbContext classs

---
+++ WpfOutlook/Model/WPFOutlookContext.cs
@@ -0,0 +1,25 @@
+﻿using Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class WPFOutlookContext : DbContext
+    {
+        public WPFOutlookContext()
+            :base("name=WPFOutlookContext")
+        {
+        }
+
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+    }
+}
