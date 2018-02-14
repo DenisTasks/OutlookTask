a88333c7d861e71292988704e321b8357dd78de7
Anton_Kudryn <Anton_Kudryn@EPBYGROW0342>
2018-02-14 14:53:30 +0300

    Added marker interfaces

---
+++ WpfOutlook/TestWpf/Interfaces/IView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModel.Interfaces;
+
+namespace TestWpf.Interfaces
+{
+    interface IView
+    {
+        IViewModel IViewModel { get; set; }
+        void Show();
+    }
+}
