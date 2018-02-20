be3f231b55f8d8047f92eb0af57f00d40d4c8676
Dzianis_Tarasevich <Dzianis_Tarasevich@EPBYGROW0335>
2018-02-20 11:26:13 +0300

    Start window type "window" changed to type "page"

---
+++ WpfOutlook/TestWpf/MainWindowPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Messaging;
+using ViewModel.Helpers;
+using ViewModel.ViewModels;
+
+namespace TestWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindowPage.xaml
+    /// </summary>
+    public partial class MainWindowPage : Page
+    {
+        public MainWindowPage()
+        {
+            InitializeComponent();
+            Messenger.Default.Register<OpenWindowMessage>(
+                this,
+                message => {
+                    if (message.Type == WindowType.AddAppWindow && message.Argument != "Load this appointment")
+                    {
+                        var addAppWindowVM = SimpleIoc.Default.GetInstance<AddAppWindowViewModel>();
+                        var addAppWindow = new AddAppWindow()
+                        {
+                            DataContext = addAppWindowVM
+                        };
+                        var result = addAppWindow.ShowDialog();
+                    }
+                    if (message.Type == WindowType.AddAboutAppointmentWindow)
+                    {
+                        var addAboutWindowVM = SimpleIoc.Default.GetInstance<AboutAppointmentWindowViewModel>();
+                        var addAboutWindow = new AboutAppWindow()
+                        {
+                            DataContext = addAboutWindowVM
+                        };
+                        Messenger.Default.Send(new OpenWindowMessage() { Argument = message.Argument, Appointment = message.Appointment });
+                        var result = addAboutWindow.ShowDialog();
+                    }
+                    if (message.Type == WindowType.None)
+                    {
+                        Messenger.Default.Send(new OpenWindowMessage() { Type = WindowType.Refresh });
+                    }
+                    if (message.Type == WindowType.AddAllAppByLocationWindow)
+                    {
+                        var addAllAppWindowVM = SimpleIoc.Default.GetInstance<AllAppByLocationWindowViewModel>();
+                        var addAllAppWindow = new AllAppByLocation()
+                        {
+                            DataContext = addAllAppWindowVM
+                        };
+                        Messenger.Default.Send(new OpenWindowMessage() { Type = WindowType.LoadLocations, Argument = message.Argument });
+                        var result = addAllAppWindow.ShowDialog();
+                    }
+                });
+        }
+    }
+}
