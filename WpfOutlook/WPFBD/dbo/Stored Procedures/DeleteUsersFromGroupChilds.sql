a2fc7eb0c8c1a7d207c9ecdc66e8b83fe4bca195
Anton_Kudryn <Anton_Kudryn@EPBYGROW0342>
2018-03-07 15:50:34 +0300

    added db

---
+++ WpfOutlook/WPFBD/dbo/Stored Procedures/DeleteUsersFromGroupChilds.sql
@@ -0,0 +1,18 @@
+﻿
+CREATE PROCEDURE [dbo].[DeleteUsersFromGroupChilds]
+	@groupId int,
+	@List [dbo].[UserList] readonly
+AS	
+BEGIN
+	WITH C (GroupID, [GROUPNAME], ParentId, NESTINGLVL) AS
+	(
+		SELECT B.GroupId, B.[GroupName], B.ParentId, 1 FROM Groups AS B WHERE ParentId = @groupId
+		UNION ALL
+		SELECT B.GroupId, B.[GroupName], B.ParentId, (NESTINGLVL +1) FROM Groups AS B INNER JOIN C ON C.GroupID = B.ParentId 
+	)
+	DELETE	FROM UserGroups 
+			WHERE UserId IN (SELECT * FROM @List)
+			AND GroupId IN (SELECT  GroupId  FROM  C )
+
+
+END
---
+++ WpfOutlook/WPFBD/dbo/User Defined Types/UserListIds.sql
@@ -0,0 +1,3 @@
+﻿CREATE TYPE [dbo].[UserListIds] AS TABLE (
+    [Id] UNIQUEIDENTIFIER NOT NULL);
+
