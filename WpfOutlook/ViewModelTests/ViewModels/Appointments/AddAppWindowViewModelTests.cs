2c986cfcd7f5979f45b8f11d0bd99ba78b4e6dfb
Dzianis_Tarasevich <Dzianis_Tarasevich@EPBYGROW0335>
2018-03-13 11:06:46 +0300

    Merge

--- WpfOutlook/ViewModelTests/ViewModels/Appointments/AddAppWindowViewModelTests.cs
+++ WpfOutlook/ViewModelTests/ViewModels/Appointments/AddAppWindowViewModelTests.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using BLL.EntitesDTO;
 using BLL.Interfaces;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
